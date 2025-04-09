71129a96969044e3f134c3596fd84668e6f42d7e
somi17k <somilkathuria1717@gmail.com>
2025-04-10 01:23:38 +0530

    server

--- server/server.js
+++ server/server.js
@@ -11,10 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/api/resources', resourceRoutes);
